6590163d1f79e339dd63c2b112142ad3875db92d
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2010-11-13 10:30:34 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'sqlite3-ruby'
+gem 'jquery-rails'
+gem 'fb_graph'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autobiographer::Application.config.secret_token = '2dc3c9e9a35a0d166ad027178cc9df9ef059d4ab8dec0d71e1fdd0a8a2aab7a4719fb9a40913a165a110b2c6d28bedd4a6adb694a6b71ea40fb08d9b2b93e147'
