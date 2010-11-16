46444787e025d16e1ee23067404154d833d2f93a
Ryan Wallace <rywall@gmail.com>
2010-11-16 14:04:56 -0600

    Twitter authenticate using oauth. Should refactor common oauth code into Authentication.

--- app/controllers/twitter_authentications_controller.rb
+++ app/controllers/twitter_authentications_controller.rb
@@ -1,12 +1,21 @@
 class TwitterAuthenticationsController < ApplicationController
   skip_before_filter :require_authentication
   
+  # handle Normal OAuth flow: start
   def new
-    @twitter_authentication = TwitterAuthentication.new
+    request_token = TwitterAuthentication.request_token(callback_twitter_authentications_url)
+    
+    session[:twitter_request_token]  = request_token.token
+    session[:twitter_request_secret] = request_token.secret
+    
+    redirect_to request_token.authorize_url
   end
 
+  # handle Normal OAuth flow: callback
   def create
-    authenticate TwitterAuthentication.add_to_user(current_user, params[:twitter_authentication][:token])
+    access_token = TwitterAuthentication.get_access_token(params[:oauth_verifier], session[:twitter_request_token], session[:twitter_request_secret])
+    authenticate TwitterAuthentication.add_to_user(current_user, access_token)
+    
     redirect_to root_url
   end
 end
--- app/models/twitter_authentication.rb
+++ app/models/twitter_authentication.rb
@@ -8,7 +8,56 @@ class TwitterAuthentication < Authentication
     end
   end
   
+  def access_token
+    @access_token ||= OAuth::AccessToken.new(self.class.consumer, self.token, self.secret)
+  end
+  
+  def self.request_token(callback_url)
+    consumer.get_request_token(:oauth_callback => callback_url)
+  end
+  
+  def self.get_access_token(verifier_token, request_token, request_secret)
+    request_token = OAuth::RequestToken.new(consumer, request_token, request_secret)
+    request_token.get_access_token(:oauth_verifier => verifier_token)
+  end
+  
   def self.identifier(access_token)
-    access_token  
+    user_info = access_token.get("http://api.twitter.com/1/account/verify_credentials.json")
+    JSON.parse(user_info.body)["name"]  
+  end
+  
+  def self.token(access_token)
+    access_token.token
+  end
+  
+  def self.secret(access_token)
+    access_token.secret
+  end
+  
+  private
+  
+  def self.consumer
+    OAuth::Consumer.new(config[:client_token], config[:client_secret], {
+      :site               => "http://api.twitter.com", 
+      :request_endpoint   => "http://api.twitter.com",
+      :scheme             => :header,
+      :http_method        => :post,
+      :request_token_path => "/oauth/request_token",
+      :access_token_path  => "/oauth/access_token",
+      :authorize_path     => "/oauth/authorize"
+      })
+  end
+  
+  def self.config
+    @config ||= if ENV['fs_client_token'] && ENV['fs_client_secret']
+      {
+        :client_token  => ENV['fs_client_token'],
+        :client_secret => ENV['fs_client_secret']
+      }
+    else
+      YAML.load_file("#{Rails.root}/config/twitter.yml")[Rails.env].symbolize_keys
+    end
+  rescue Errno::ENOENT => e
+    raise StandardError.new("config/twitter.yml could not be loaded.")
   end
 end
