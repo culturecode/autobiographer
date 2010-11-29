d7a21be7077f8e991048cbc150ca1ff56afc5034
Ryan Wallace <rywall@gmail.com>
2010-11-28 19:24:54 -0500

    Give PhotoGroups an identifier.

--- db/migrate/20101116193013_create_photo_groups.rb
+++ db/migrate/20101116193013_create_photo_groups.rb
@@ -1,6 +1,8 @@
 class CreatePhotoGroups < ActiveRecord::Migration
   def self.up
     create_table :photo_groups do |t|
+      t.text :identifier # The id of the object on the service        
+      
       t.belongs_to :authentication # The service the event was grabbed from
       
       t.timestamps
