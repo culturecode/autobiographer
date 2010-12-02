b0eadf6d53db4e24c66f11b7444f35e0959ecf4d
Ryan Wallace <rywall@gmail.com>
2010-12-01 21:43:12 -0500

    Use the correct date time from EXIF data. Closes #22.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,7 +1,7 @@
 class UploadsController < ApplicationController
   def photos
     photo = Photo.new(:file => params[:file])
-    photo_timestamp = EXIFR::JPEG.new(photo.file.path).date_time
+    photo_timestamp = EXIFR::JPEG.new(photo.file.path).date_time_original
     
     photo_group = Event.where(:details_type => "PhotoGroup", :authentication_id => current_user.autobiographer_authentication.id, :timestamp => (photo_timestamp - 1.hour)..(photo_timestamp + 1.hour)).first.try(:details)
     photo_group ||= PhotoGroup.create(:authentication => current_user.autobiographer_authentication, :timestamp => photo_timestamp)
