1bf1349036d2b49e04babbc2419926c6d8e7b544
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2010-12-01 21:03:07 -0800

    more styling. getting chapter creation and deletion to work again.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,32 +1,5 @@
 module EventsHelper
-  def event_spacer(event, previous_event = nil)
-    content = "".html_safe.tap do |output|    
-      case event.details
-      when Chapter, Note
-      else
-        output << chapter_breaker(event) unless previous_event.details.is_a?(Chapter) || event.happened_same_day_as(previous_event)
-      end
-    end
 
-    content_tag :li, content, :class => 'event_spacer', :id => "event_#{event.id}_spacer" unless content.blank?
-  end
-
-  def chapter_breaker(event)
-    link_to split_chapter_path(:event_id => event.id), :class => 'chapter_breaker', :remote => true do
-      content_tag(:div, '', :class => :line) + content_tag(:span, 'Split Chapter', :class => :text)
-    end
-  end
-
-  def annotation_inserter(event)
-    link_to create_note_path(:event_id => event.id), :class => 'annotation_inserter', :remote => true do
-      content_tag(:div, '', :class => :line) + content_tag(:span, 'Add Note', :class => :text)
-    end
-  end
-  
-  def delete_event_link(event_details)
-    link_to image_tag('blank.gif'), polymorphic_path(event_details), :method => :delete, :class => :delete_event_link, :remote => true
-  end
-  
   def event_list_timestamp(event, previous_event)
     if event.happened_same_day_as(previous_event) && !previous_event.details.is_a?(Chapter)
       if (event.timestamp - previous_event.timestamp) < 3.hours
@@ -62,6 +35,19 @@ module EventsHelper
     if event.authentication.present?
       service_name = event.authentication.service_name
       image_tag("services/#{service_name}_tiny.png", :title => service_name.titleize, :class => "service_icon")
+    end
+  end
+  
+  def event_new_chapter_link(event)
+    link_to('Start a new chapter', split_chapter_path(:event_id => event.id), :remote => true, :class => 'new_chapter_link')
+  end
+  
+  def event_class(event)
+    case event.details
+    when Chapter
+      'chapter'
+    else
+      'event'
     end
   end
 end
