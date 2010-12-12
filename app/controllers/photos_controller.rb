151fb8a4092e22e3b15a322ff5869f4aaa380576
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2010-12-11 18:28:18 -0800

    Changed slogan. Images without datestamps now appear at the end of the timeline. Closes #9.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,10 +3,21 @@ class PhotosController < ApplicationController
     photo = Photo.new(:file => params[:file])
     photo_timestamp = EXIFR::JPEG.new(photo.file.path).date_time_original
     
+    # Give the photo a timestamp if it doesn't have one
+    if photo_timestamp.nil?
+      no_timestamp = true 
+      photo_timestamp = Time.now
+    end
+    
     photo_group = Event.where(:details_type => "PhotoGroup", :authentication_id => current_user.autobiographer_authentication.id, :timestamp => (photo_timestamp - 1.hour)..(photo_timestamp + 1.hour)).first.try(:details)
     photo_group ||= PhotoGroup.create(:authentication => current_user.autobiographer_authentication, :timestamp => photo_timestamp)
-    
     photo_group.photos << photo
+    
+    # If the photo didn't have a timestamp, and if the photo group it went into doesn't have a note,
+    # make one that tells the user that the image had no timestamp
+    if no_timestamp && photo_group.note.nil?
+      photo_group.create_note(:text => "These photos didn't have any date information, so they went here instead. In a later version you will be able to put these photos where they belong.")
+    end
     
     render :nothing => true
   end
